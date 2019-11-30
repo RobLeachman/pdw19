bf58f8ae3138f53ac54a9734cc6d5fe7f7b6f3dc
Rob Leachman <rob@bitblaster.com>
2019-11-30 15:58:29 -0800

    Get things and build stuff

--- pdw.js
+++ pdw.js
@@ -1,50 +1,54 @@
-/* Produce, Defend, Win!
+/* Produce, Defend, Win! 2019 edition
  *
- * A man walks a road...
- *    BUG: ... and if he's moving he can fly
+ * A man walks a road,
+ * driven by keypresses or swipes,
+ * takes stuff and builds things,
+ * somewhat ready to fail altogether
+ * (BUG) like when he gets over to the drama stage
+ *
  */
+var funZoom = true;
 
 /* global Phaser */
 var game;
 var gameOptions = { // will want these soon
     difficulty: 0,
-    testing: 1
+    testing: 1,
+
+    swipeMaxTime: 1000,
+    swipeMinDistance: 20,
+    swipeMinNormal: 0.85
 };
 
 const map = [
-          [[35,100,1], [100,100], [165,100,2], [230,100], [295,100,3], [360,100]],
-          [[35,230,4], [100,230], [165,230,5], [230,230], [295,230,6], [360,230]],
-          [[35,360,7], [100,360], [165,360,8], [230,360], [295,360,9], [360,360]],
-          [[-1,-1],  [-1,-1],   [-1,-1],   [-1,-1],   [-1,-1],   [360,540], [425,540,10], [545,540,11], [665,540,12]]
+          [[35,100,0], [100,100], [165,100,1], [230,100], [295,100,2], [360,100]],
+          [[35,230,3], [100,230], [165,230,4], [230,230], [295,230,5], [360,230]],
+          [[35,360,6], [100,360], [165,360,7], [230,360], [295,360,8], [360,360]],
+          [[-1,-1], [-1,-1], [-1,-1], [-1,-1], [-1,-1],                [360,540], [425,540,10], [545,540,11], [665,540,12]]
             ];
 
 const LEFT = 0;
 const RIGHT = 1;
 const UP = 2;
 const DOWN = 3;
+const INTERACT = 4;
 
-function getMapCoords(goalPoint) {
-    try {
-        if (goalPoint.x < 0 || goalPoint.y < 0)
-            return null;
-        if (map[goalPoint.y][goalPoint.x][0] < 0)
-            return null;
-        if (map[goalPoint.y][goalPoint.x][1] < 0)
-            return null;
-        return new Phaser.Geom.Point(map[goalPoint.y][goalPoint.x][0], map[goalPoint.y][goalPoint.x][1]);
-    } catch (err) { // array out of bounds
-        return null;
-    }
-}
+const NOTHING = 0;
+const THING = 1;
+const GAS = 2;
+
+var world = [];
+var didFail = false;
+
 
 /**************************************
- * INIT
+ * ENGINE START
  **************************************/
 window.onload = function() {
     var gameConfig = {
         width: 800,
         height: 600,
-        backgroundColor: 0x333333,
+        backgroundColor: 0x000000,
         scene: [bootGame, playGame]
     };
     game = new Phaser.Game(gameConfig);
@@ -59,10 +63,48 @@ class bootGame extends Phaser.Scene{
     }
     preload(){
         this.load.image("road", "assets/graphics/road3.png");
-        this.load.spritesheet("man", "assets/graphics/man.png", {
+        this.load.spritesheet("man", "assets/graphics/aMan.png", {
             frameWidth: 40,
             frameHeight: 40
         });
+        this.load.spritesheet("manWithThing", "assets/graphics/manWithThing.png", {
+            frameWidth: 40,
+            frameHeight: 40
+        });
+
+        this.load.spritesheet("base", "assets/graphics/base.png", {
+            frameWidth:90,
+            frameHeight:90
+        });
+        this.load.spritesheet("fuelStore", "assets/graphics/fuelStore1A.png", {
+            frameWidth:25,
+            frameHeight:25
+        });
+        this.load.spritesheet("botFactoryPad", "assets/graphics/botFactoryPad.png", {
+            frameWidth:90,
+            frameHeight:90
+        });
+        this.load.spritesheet("botFactory", "assets/graphics/botFactory.png", {
+            frameWidth:90,
+            frameHeight:90
+        });
+        this.load.spritesheet("gasFactoryPad", "assets/graphics/gasFactoryPad.png", {
+            frameWidth:90,
+            frameHeight:90
+        });
+        this.load.spritesheet("gasFactory", "assets/graphics/gasFactory.png", {
+            frameWidth:90,
+            frameHeight:90
+        });
+        this.load.spritesheet("generator", "assets/graphics/generator0.png", {
+            frameWidth:90,
+            frameHeight:90
+        });
+        this.load.spritesheet("pad", "assets/graphics/pad1.png", {
+            frameWidth:90,
+            frameHeight:90
+        });
+
     }
     create(){
         this.scene.start("PlayGame");
@@ -73,83 +115,323 @@ class playGame extends Phaser.Scene{
         super("PlayGame");
     }
     create(){
+/**************************************
+ * INIT
+ **************************************/
+        //this.cameras.main.setBounds(0,0,800,600);
+        this.cameras.main.setZoom(1.55);
+        this.cameras.main.setScroll(-200,-95);
         this.add.image(0, 0, "road").setOrigin(0, 0);
 
-        this.manLoc = new Phaser.Geom.Point(2,1);
+        for (var spot=0;spot<9;spot++) {
+            if (spot == 0) {
+               var botFactory = new BotFactory(this,"botFactoryPad", spot);
+               world.push(botFactory);
+            } else if (spot == 4) {
+               var base = new Base(this,"base", spot, "fuelStore");
+               world.push(base);
+            } else if (spot == 8) {
+               var gasFactory = new GasFactory(this,"gasFactoryPad", spot);
+               world.push(gasFactory);
+            } else {
+               var loc = new Generator(this,"pad",spot);
+               world.push(loc);
+            }
+        }
 
-        var manCoords = getMapCoords(this.manLoc);
+        this.man = {
+            location: new Phaser.Geom.Point(2,1),
+            spot: 4,
+            moving: false,
+            moveBuffer: -1,
+            carrying: NOTHING
+        };
 
-        this.man = this.add.sprite(manCoords.x, manCoords.y, "man", 0).setOrigin(0,0);
+        var manCoords = getMapCoords(this.man.location);
+
+        this.man.sprite = this.add.sprite(manCoords.x, manCoords.y, "man", 0).setOrigin(0,0);
 
         this.input.keyboard.on("keyup", this.handleKey, this);
+        this.input.on("pointerup", this.handleSwipe, this);
     }
     update () {
-        // no loop needed, the keyboard handler is sufficient for now
+        try {
+            if (!this.man.moving && this.man.moveBuffer > -1) {
+                if (this.man.moveBuffer == INTERACT) {
+
+
+                    var manAt = map[this.man.location.y][this.man.location.x][2];
+                    console.log("Interact at " + manAt);
+                    if (typeof manAt != "undefined")
+                        world[manAt].interact(this.man);
+                } else
+                    this.makeMove(this.man.moveBuffer);
+                this.man.moveBuffer = -1;
+            }
+        } catch (err) {
+            // the shit's gonna fail, try to capture a clue when it does...
+            // not too bad on PC but harder on phone and this attmept is sad
+            if (didFail) {
+                throw ("the wheels, they have come off the wagon again")
+            }
+            console.log("ERROR " + err);
+            console.log("STACK " + err.stack);
+            didFail = true;
+
+            var ratfarts = this.add.graphics();
+            ratfarts.fillStyle(0x0000FF, 1);
+            var fail = new Phaser.Geom.Rectangle(0,0,800,600);
+            ratfarts.fillRectShape(fail);
+            this.add.text(0, 40, err,{ font: '18px Courier' });
+            this.add.text(0, 70, "RATFARTS",{ font: '18px Courier' });
+            this.add.text(0, 100, err.stack,{ font: '18px Courier' });
+        }
     }
 
     handleKey(e){
-        if(1) {
-            switch(e.code){
-                case "KeyA":
-                case "ArrowLeft":
-                    this.makeMove(LEFT);
-                    break;
-                case "KeyD":
-                case "ArrowRight":
-                    this.makeMove(RIGHT);
-                    break;
-                case "KeyW":
-                case "ArrowUp":
-                    this.makeMove(UP);
-                    break;
-                case "KeyS":
-                case "ArrowDown":
-                    this.makeMove(DOWN);
-                    break;
+        switch(e.code){
+            case "KeyA":
+            case "ArrowLeft":
+                this.man.moveBuffer = LEFT;
+                break;
+            case "KeyD":
+            case "ArrowRight":
+                this.man.moveBuffer = RIGHT;
+                break;
+            case "KeyW":
+            case "ArrowUp":
+                this.man.moveBuffer = UP;
+                break;
+            case "KeyS":
+            case "ArrowDown":
+                this.man.moveBuffer = DOWN;
+                break;
+            case "Space":
+                this.man.moveBuffer = INTERACT;
+        }
+    }
+
+    handleSwipe(e){
+        var swipeTime = e.upTime - e.downTime;
+        var fastEnough = swipeTime < gameOptions.swipeMaxTime;
+        var swipe = new Phaser.Geom.Point(e.upX - e.downX, e.upY - e.downY);
+        var swipeMagnitude = Phaser.Geom.Point.GetMagnitude(swipe);
+        var longEnough = swipeMagnitude > gameOptions.swipeMinDistance;
+        if(longEnough && fastEnough){
+            Phaser.Geom.Point.SetMagnitude(swipe, 1);
+            if(swipe.x > gameOptions.swipeMinNormal){
+                this.man.moveBuffer = RIGHT;
             }
+            if(swipe.x < -gameOptions.swipeMinNormal){
+                this.man.moveBuffer = LEFT;
+            }
+            if(swipe.y > gameOptions.swipeMinNormal){
+                this.man.moveBuffer = DOWN;
+            }
+            if(swipe.y < -gameOptions.swipeMinNormal){
+                this.man.moveBuffer = UP;
+            }
+        } else {
+            console.log('tap');
+            this.man.moveBuffer = INTERACT;
         }
     }
 
     makeMove(dir) {
-        var oldLocX = this.manLoc.x; // copying these 2 attributes only... love JS
-        var oldLocY = this.manLoc.y;
-        var manAt = map[this.manLoc.y][this.manLoc.x][2];
+        var oldLocX = this.man.location.x; // copying these 2 attributes only... love JS
+        var oldLocY = this.man.location.y;
+        var manAt = map[this.man.location.y][this.man.location.x][2];
         switch (dir) {
             case LEFT:
-                this.manLoc.x--;
+                this.man.location.x--;
                 break;
             case RIGHT:
-                this.manLoc.x++;
+                this.man.location.x++;
                 break;
             case UP:
-                this.manLoc.y--;
-                if (manAt > 0)
-                   this.manLoc.y = -1;
+                this.man.location.y--;
+                if (manAt > -1)
+                   this.man.location.y = -1;
                 break;
             case DOWN:
-                this.manLoc.y++;
-                if (manAt > 0)
-                   this.manLoc.y = -1;
+                this.man.location.y++;
+                if (funZoom && this.man.location.y > 2) {
+                    funZoom = false;
+                    var cam = this.cameras.main;
+                    cam.pan(400, 300, 400, 'Linear');
+                    cam.zoomTo(1, 400);
+                }
+                if (manAt > -1) {
+                   this.man.location.y = -1;
+                }
                 break;
         }
-        var newCoords = getMapCoords(this.manLoc);
+        var newCoords = getMapCoords(this.man.location);
         if (!newCoords) {
-            this.manLoc.x = oldLocX;
-            this.manLoc.y = oldLocY;
+            this.man.location.x = oldLocX;
+            this.man.location.y = oldLocY;
         }
         else {
-            var manCoords = getMapCoords(this.manLoc);
+            var manCoords = getMapCoords(this.man.location);
+            this.man.moving = true;
             this.tweens.add({
-                targets: [this.man],
-                duration: 500,
+                targets: [this.man.sprite],
+                duration: 300,
                 x:manCoords.x,
                 y:manCoords.y,
                 callbackScope: this,
                 onComplete: function() {
-                    //console.log("DONE");
+                    this.man.moving = false;
                 }
             });
         }
+    }
+}
+
+function BotFactory(game, spriteName, spot) {
+    this.spot = spot;
+    this.sprite = game.add.sprite(getLocationX(spot), getLocationY(spot), spriteName, 0).setOrigin(0,0);
+
+    this.interact = function (theMan) {
+        if (!this.built) {
+           if (theMan.carrying == THING) {
+              this.sprite = game.add.sprite(getLocationX(spot), getLocationY(spot), "botFactory", 0).setOrigin(0,0);
+
+              theMan.sprite.setFrame(0);
+              theMan.carrying = NOTHING;
+              this.built = true;
+           }
+        }
+    };
+}
+
+function GasFactory(game, spriteName, spot) {
+    this.spot = spot;
+    this.sprite = game.add.sprite(getLocationX(spot), getLocationY(spot), spriteName, 0).setOrigin(0,0);
+
+    this.interact = function (theMan) {
+        if (!this.built) {
+           if (theMan.carrying == THING) {
+              this.sprite = game.add.sprite(getLocationX(spot), getLocationY(spot), "gasFactory", 0).setOrigin(0,0);
+
+              theMan.sprite.setFrame(0);
+              theMan.carrying = NOTHING;
+              this.built = true;
+           }
+        }
+    };
+}
+
+function Generator(game, spriteName, spot) {
+    this.spot = spot;
+    this.sprite = game.add.sprite(getLocationX(spot), getLocationY(spot), spriteName, 0).setOrigin(0,0);
+    this.built = false;
+
+    this.interact = function (theMan) {
+        if (!this.built) {
+           if (theMan.carrying == THING) {
+              this.sprite = game.add.sprite(getLocationX(spot), getLocationY(spot), "generator", 0).setOrigin(0,0);
+
+              theMan.sprite.setFrame(0);
+              theMan.carrying = NOTHING;
+              this.built = true;
+           }
+        }
+    };
+}
+
+function Base(game, spriteName, spot, fuelStoreSprite) {
+    this.spot = spot;
+    this.sprite = game.add.sprite(getLocationX(spot), getLocationY(spot), spriteName, 0).setOrigin(0,0);
+
+    this.fuelStoreSprite = fuelStoreSprite;
+    this.fuelBay = [10,10,10];
+    this.fuelSprite = [];
+    this.fuelSprite[1] = game.add.sprite(146,175,this.fuelStoreSprite,this.fuelBay[1]).setOrigin(0,0);
+    this.fuelSprite[0] = game.add.sprite(173,175,this.fuelStoreSprite,this.fuelBay[0]).setOrigin(0,0);
+    this.fuelSprite[2] = game.add.sprite(200,175,this.fuelStoreSprite,this.fuelBay[2]).setOrigin(0,0);
+
+    this.interact = function (theMan) {
+        if (theMan.carrying == NOTHING) {
+           if (this.takeStuff()) {
+               theMan.sprite.setFrame(1);
+               theMan.carrying = THING;
+           }
+        } else if (theMan.carrying == THING) {
+           if (this.stashStuff()) {
+              theMan.sprite.setFrame(0);
+              theMan.carrying = NOTHING;
+           }
+        }
+    };
+
+    this.takeStuff = function () {
+        for (var x=0;x<3;x++) {
+            if (this.fuelBay[x] > 0) {
+               this.fuelBay[x] = 0;
+               this.repaint();
+               return true;
+            }
+        }
+        return false;
+    }
+
+    this.stashStuff = function () {
+        for (var x=0;x<3;x++) {
+            if (this.fuelBay[x] == 0) {
+               this.fuelBay[x] = 10;
+               this.repaint();
+               return true;
+            }
+        }
+        return false;
+    }
+
+    this.repaint = function() {
+           this.fuelSprite[0].setFrame(this.fuelBay[0]);
+           this.fuelSprite[1].setFrame(this.fuelBay[1]);
+           this.fuelSprite[2].setFrame(this.fuelBay[2]);
+    }
+}
+
+/***********************************
+ * MISC
+ ***********************************/
+
+// "Translate screen locations into screen coordinates"
+function getLocationX(location) {
+    var coordX = 0;
+    if (location == 0 || location == 3 || location == 6)
+        coordX = 10;
+    if (location == 1 || location == 4 || location == 7)
+        coordX = 140;
+    if (location == 2 || location == 5 || location == 8)
+        coordX = 270;
+    return coordX;
+}
+function getLocationY(location) {
+    var coordY = 0;
+
+    if (location == 0 || location == 1 || location == 2)
+        coordY = 10;
+    if (location == 3 || location == 4 || location == 5)
+        coordY = 140;
+    if (location == 6 || location == 7 || location == 8)
+        coordY = 270;
+    return coordY;
+}
+
+function getMapCoords(goalPoint) {
+    try {
+        if (goalPoint.x < 0 || goalPoint.y < 0)
+            return null;
+        if (map[goalPoint.y][goalPoint.x][0] < 0)
+            return null;
+        if (map[goalPoint.y][goalPoint.x][1] < 0)
+            return null;
+        return new Phaser.Geom.Point(map[goalPoint.y][goalPoint.x][0], map[goalPoint.y][goalPoint.x][1]);
+    } catch (err) { // array out of bounds
+        return null;
     }
 }
 
